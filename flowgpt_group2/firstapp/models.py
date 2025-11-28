1e715049c4250b4bfc9b67675873da1aa9dbed44
xtianeugene <wanzalaeugenefredrick@gmail.com>
2025-11-28 18:25:52 +0300

    5th Commit

--- flowgpt_group2/firstapp/models.py
+++ flowgpt_group2/firstapp/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    def __str__(self):
+        return self.name
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=200)
+    contact_info = models.TextField(blank=True)
+    address = models.TextField(blank=True)
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField()
+    stock_quantity = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone_number = models.CharField()
+    address = models.TextField(blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('PROCESSING', 'Processing'),
+        ('COMPLETED', 'Completed'),
+        ('CANCELED', 'Canceled'),
+    ]
+    METHOD_CHOICES = [
+        ('MPESA', 'Mpesa'),
+        ('PAYPAL', 'Paypal'),
+        ('BANK', 'Bank'),
+        ('Crypto', 'Crypto'),
+    ]
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(choices=STATUS_CHOICES, default='PENDING', max_length=10)
+    payment_method = models.CharField(choices=METHOD_CHOICES, default='MPESA', max_length=10)
+    def __str__(self):
+        return self.customer.name
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    def get_total_price(self):
+            return self.quantity * self.price
+
+    def __str__(self):
+            return f"({self.quantity} * {self.Product.name})"
